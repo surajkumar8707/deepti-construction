bca3f0772506c8cd53dd8480d9b31d10ff6e2c1d
Suraj Verma <suraj@nextolive.com>
2024-01-15 23:46:39 +0530

    worked on Basic setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class, 'index'])->name('home');
+Route::get('architects-in-hosur',[HomeController::class, 'architectsInHosur'])->name('architects.in.hosur');
+Route::get('architects-in-bangalore',[HomeController::class, 'architectsInBangalore'])->name('architects.in.bangalore');
+Route::get('architects-in-chennai',[HomeController::class, 'architectsInChennai'])->name('architects.in.chennai');
+Route::get('building-construction-contractors-hosur',[HomeController::class, 'buildingConstructionContractorsHosur'])->name('building.construction.contractors.hosur');
+Route::get('building-construction-contractors-bangalore',[HomeController::class, 'buildingConstructionContractorsBangalore'])->name('building.construction.contractors.bangalore');
+Route::get('building-construction-contractors-chennai',[HomeController::class, 'buildingConstructionContractorsChennai'])->name('building.construction.contractors.chennai');
+Route::get('interior-designers-in-hosur',[HomeController::class, 'interiorDesignersInHosur'])->name('interior.designers.in.hosur');
+Route::get('interior-designers-in-bangalore',[HomeController::class, 'interiorDesignersInBangalore'])->name('interior.designers.in.bangalore');
+Route::get('interior-designers-in-chennai',[HomeController::class, 'interiorDesignersInChennai'])->name('interior.designers.in.chennai');
+Route::get('house-construction-company-in-hosur',[HomeController::class, 'houseConstructionCompanyInHosur'])->name('house.construction.company.in.hosur');
+Route::get('house-construction-company-in-bangalore',[HomeController::class, 'houseConstructionCompanyInBangalore'])->name('house.construction.company.in.bangalore');
+Route::get('house-construction-company-in-chennai',[HomeController::class, 'houseConstructionCompanyInChennai'])->name('house.construction.company.in.chennai');
+Route::get('contact-sales',[HomeController::class, 'contactSales'])->name('contact.sales');
+Route::get('about-us',[HomeController::class, 'aboutUs'])->name('about.us');
+Route::get('get-quote',[HomeController::class, 'getQuote'])->name('get.quote');
